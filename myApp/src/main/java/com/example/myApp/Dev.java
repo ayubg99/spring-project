e1608a9970e0b3a6f9048c7dfb4ced455d163605
Ayoub <ayubgharbi@gmail.com>
2025-08-26 14:52:58 +0200

    autowiring using spring boot

--- myApp/src/main/java/com/example/myApp/Dev.java
+++ myApp/src/main/java/com/example/myApp/Dev.java
@@ -1,8 +1,24 @@
 package com.example.myApp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Dev {
 
+    @Autowired
+    @Qualifier("laptop")
+    private Computer comp;
+
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
     public void build(){
+
+        comp.compile();
+
         System.out.println("working on Awesome Project");
     }
 }
--- myApp/src/main/java/com/example/myApp/MyAppApplication.java
+++ myApp/src/main/java/com/example/myApp/MyAppApplication.java
@@ -2,14 +2,16 @@ package com.example.myApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyAppApplication {
 
 	public static void main(String[] args) {
 
-        SpringApplication.run(MyAppApplication.class, args);
-        Dev obj = new Dev();
+        ApplicationContext context = SpringApplication.run(MyAppApplication.class, args);
+
+        Dev obj = context.getBean(Dev.class);
 
         obj.build();
 	}
